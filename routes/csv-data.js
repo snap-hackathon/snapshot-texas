3b65ee941449c237f35c0c726481f40dfc1dbc86
Dylan Smith <dylantsmith@gmail.com>
2014-05-31 14:35:01 -0500

    Add csv data route

---
+++ routes/csv-data.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var csv = require("csv");
+
+module.exports.something = {
+    handler: function(request, reply) {
+        reply("hello!");
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 "use strict";
 
+var csvData = require("./csv-data");
+
 module.exports.endpoints = [
+    { method: "GET", path: "/api/something", config: csvData.something }
 ];
