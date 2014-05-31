c0cf5fafa3077327fd6ae1a4b95235125a2c41fc
Thien Vo <vohuythien@utexas.edu>
2014-05-31 16:17:29 -0500

    changed the data parsing to a function

--- routes/csv-data.js
+++ routes/csv-data.js
@@ -2,39 +2,58 @@
 
 var csv = require("csv");
 
+function read_file(file_name, callback) {
+    var names = {};
+    csv().from.path(__dirname + "/../Data/" + file_name, {
+        delimiter: ",",
+        escape: '"'
+    })
+
+    // when a record is found in the CSV file (a row)
+    .on("record", function(row, index) {
+        var zip, lastName;
+        // skip the header row
+        if (index === 0) {
+            return;
+        }
+
+        // read in the data from the row
+        zip = row[2].trim();
+        //lastName = row[1].trim();
+        console.log(zip);
+        if (zip !== "78705")
+            return;
+
+        
+
+        names.push(row);
+        // perform some operation with the data
+        // ...
+
+
+    })
+    // when the end of the CSV document is reached
+    .on("end", function() {
+
+        callback(names);
+    })
+    // if any errors occur
+    .on("error", function(error) {
+        console.log(error.message);
+    });
+}
+
+
 module.exports.something = {
     handler: function(request, reply) {
-    	var names = [];
-        csv().from.path(__dirname + "/../Data/Cost_of_Food.csv", {
-            delimiter: ",",
-            escape: '"'
-        })
-        // when a record is found in the CSV file (a row)
-        .on("record", function(row, index) {
-            var firstName, lastName;
-
-            // skip the header row
-            if (index === 0) {
-                return;
-            }
-
-            // read in the data from the row
-            firstName = row[0].trim();
-            lastName = row[1].trim();
-            names.push(firstName);
-            // perform some operation with the data
-            // ...
-
-
-        })
-        // when the end of the CSV document is reached
-        .on("end", function() {
-        	
-            reply(names);
-        })
-        // if any errors occur
-        .on("error", function(error) {
-            console.log(error.message);
+        var file_name = ["SNAP_Particpation_and_Race_Merged.csv", "Cost_of_Food.csv", "Food_Banks.csv"];
+        var allNames = [];
+        read_file(file_name[0], function(names){
+            allNames.push(names);
+            read_file(file_name[1], function(names) {
+                allNames.push(names);
+            });
+                reply(allNames);
         });
     }
 };
