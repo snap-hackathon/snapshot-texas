040043d5af8086ad097cb3df5838bf301c10ad47
Thien Vo <vohuythien@utexas.edu>
2014-05-31 19:48:58 -0500

    county zipcode mapping

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var csvData = require("./csv-data");
 
 module.exports.endpoints = [
     { method: "GET", path: "/api/zipcode-data/{zip}", config: csvData.dataZip },
+    { method: "GET", path: "/api/county-data/{county}", config: csvData.dataCounty },
     { method: "GET", path: "/app/{path*}", handler: {
         view: {
             template: "index.html"
